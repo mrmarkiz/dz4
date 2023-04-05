f72e1a7d7b97bb6472c37f8effdf28d99a405de6
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-05 17:26:05 +0300

    Add main

--- dz4/Program.cs
+++ dz4/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace dz4
+﻿using System.Threading.Tasks;
+
+namespace dz4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choice;
+            Console.Write("Enter task to run: ");
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error collapsed: {ex.Message}");
+                return;
+            }
+            switch (choice)
+            {
+                case 1:
+                    Task1_2.Run1();
+                    break;
+                case 2:
+                    Task1_2.Run2();
+                    break;
+                case 3:
+                    Task3_4.Run3();
+                    break;
+                case 4:
+                    Task4_4.Run4();
+                    break;
+            }
         }
     }
 }
